eb0dca36030b8adfad758fe90e12e4b5290bbc69
Administrator <605601637@qq.com>
2020-12-23 17:42:27 +0800

    12-23 完善结算单添加药品界面

--- src/main/java/com/hospital_purchase/controller/PaymentDrugMyController.java
+++ src/main/java/com/hospital_purchase/controller/PaymentDrugMyController.java
@@ -1,6 +1,7 @@
 package com.hospital_purchase.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.hospital_purchase.pojo.User;
 import com.hospital_purchase.service.PaymentDrugService;
 import com.hospital_purchase.vo.PayDrugVo;
 import com.hospital_purchase.vo.PaymentVo;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("paydrug")
@@ -20,8 +23,15 @@ public class PaymentDrugMyController {
     @ResponseBody
     public PageInfo<PayDrugVo> findAllPayDrugVo(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum,
                                               @RequestParam(value = "pageSize",defaultValue = "10") Integer pageSize,
-                                              PayDrugVo payDrugVo){
+                                              PayDrugVo payDrugVo,HttpSession httpSession){
+        payDrugVo.setHospitalId(getHospitalId(httpSession));
+
         PageInfo<PayDrugVo> payments = paymentDrugService.fandAll(pageNum, pageSize, payDrugVo);
         return payments;
+    }
+    
+    public Integer getHospitalId(HttpSession httpSession){
+        User admin = (User) httpSession.getAttribute("admin");
+        return admin.getHospitalId();
     }
 }
